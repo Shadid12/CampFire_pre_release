78b7a54738002d7a97f2089076c1b25daffff9a4
shadid12 <shadidhaque2014@gmail.com>
2017-05-17 23:47:55 -0400

    first step done

---
+++ frontend/ui/src/components/Main.js
@@ -0,0 +1,9 @@
+import { Component } from 'react'
+
+export class Main extends Component {
+	render(){
+	    return(
+	        <div>Hello Main Component</div>
+	   );
+	}
+} 
--- frontend/ui/src/index.js
+++ frontend/ui/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import { render } from 'react-dom'
+// react-routing 
+import { Route, Router, IndexRoute, hashHistory }  from 'react-router';
+
+// importing components
+import { Main } from './components/Main'
+
+
 
 window.React = React
 
 render(
-	<div>
-		Hello worlds
-	</div>,
+	<Router history={hashHistory}>
+		<Route path="/" component={Main}>
+		</Route>
+	</Router>,
 	document.getElementById('page')
 )
