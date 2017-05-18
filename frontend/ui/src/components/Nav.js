b4f31ca3188fc34cfebc242730e63defb3de249e
shadid12 <shadidhaque2014@gmail.com>
2017-05-18 01:02:04 -0400

    nav styling done

--- frontend/ui/src/components/Nav.js
+++ frontend/ui/src/components/Nav.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router'
 export class Nav extends Component{
 	render(){
 		return(
-			<div>
-			<h1>I am the nav component</h1>
-			<ul>
-				<li> <Link to="/"> Home </Link> </li>
-				<li> <Link to="/signup"> SignUp </Link> </li>
-				<li> <Link to="/login"> Login </Link> </li>
-			</ul>
-			</div>
+	  <nav>
+	    <div className="nav-wrapper light-blue darken-1">
+	      <a href="#" className="brand-logo">CampFire</a>
+	      <ul id="nav-mobile" className="right hide-on-med-and-down">
+	        <li> <Link to="/"> Home </Link> </li>
+	        <li><Link to="/signup"> SignUp </Link> </li>
+	        <li><Link to="/login"> Login </Link></li>
+	      </ul>
+	    </div>
+	  </nav>
 		)
 	}
 }
