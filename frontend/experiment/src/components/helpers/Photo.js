2585fe24cb05f7c726c134093957c1e6429b1b62
shadid12 <shadidhaque2014@gmail.com>
2017-05-20 19:09:28 -0400

    presentable gallery with top back button

--- frontend/experiment/src/components/helpers/Photo.js
+++ frontend/experiment/src/components/helpers/Photo.js
@@ -9,32 +9,46 @@ export class Photo extends Component{
 		
 	}
 
+	goBack(){
+		$("#sslist").fadeOut('slow', () => {
+			window.location.href = "/";
+		});
+	}
+
 	render(){
 		return(
-						  <ul className="grid cs-style-2">
-        <li>
-          <figure>
-            <img src="https://d13yacurqjgara.cloudfront.net/users/5276/screenshots/2172548/jd_weaver_illustration_kendrickkidd_1x.jpg" alt="img02"/>
-          </figure>
-        </li>
-        <li>
-          <figure>
-            <img src="https://d13yacurqjgara.cloudfront.net/users/68544/screenshots/2172398/drop_1x.png" alt="img04"/>
-            
-          </figure>
-        </li>
-        <li>
-          <figure>
-            <img src="https://d13yacurqjgara.cloudfront.net/users/485324/screenshots/2172192/fitmob_1x.jpg" alt="img06"/>
-            
-          </figure>
-        </li>
-        <li>
-          <figure>
-            <img src="https://d13yacurqjgara.cloudfront.net/users/77241/screenshots/2172501/days-block-print-dribbb_1x.jpg" alt="img05"/>
-          </figure>
-        </li>
-			    </ul>
+		<div id="sslist">
+
+		  <button className="button type1" onClick={this.goBack}>
+		    Go Back
+		  </button>
+
+			<ul className="grid cs-style-2">
+			<li>
+			<figure>
+			<img src="https://d13yacurqjgara.cloudfront.net/users/5276/screenshots/2172548/jd_weaver_illustration_kendrickkidd_1x.jpg" alt="img02"/>
+			</figure>
+			</li>
+			<li>
+			<figure>
+			<img src="https://d13yacurqjgara.cloudfront.net/users/68544/screenshots/2172398/drop_1x.png" alt="img04"/>
+
+			</figure>
+			</li>
+			<li>
+			<figure>
+			<img src="https://d13yacurqjgara.cloudfront.net/users/485324/screenshots/2172192/fitmob_1x.jpg" alt="img06"/>
+
+			</figure>
+			</li>
+			<li>
+			<figure>
+			<img src="https://d13yacurqjgara.cloudfront.net/users/77241/screenshots/2172501/days-block-print-dribbb_1x.jpg" alt="img05"/>
+			</figure>
+			</li>
+			</ul>
+
+		</div>
 		)
 	}
 }
